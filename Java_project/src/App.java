5fa9a7645b27d974354f1a912f60e77a5a2d124a
geoffrey88 <geoffrey.stevens@student.tdsb.on.ca>
2024-03-27 13:49:13 -0400

    inital commit

---
+++ Java_project/src/App.java
@@ -0,0 +1,8 @@
+//this page was created by Geoffrey:()
+//on "thursday March 21st 2024"
+//To "create a class for our first java app"
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
